047ef2d2ece05dec87721c44158511432fea0984
fernandokbs <fernandokbs@users.noreply.github.com>
2023-06-21 23:44:17 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,27 +3,27 @@
 use Fernandokbs\ShellBuilder\ShellBuilderClass;
 
 it('can add flag', function () {
-    $command = ShellBuilderClass::create("ruby")
-                    ->withFlag("-v")
-                    ->getExecuteCommand();
+    $command = ShellBuilderClass::create('ruby')
+        ->withFlag('-v')
+        ->getExecuteCommand();
 
-    expect($command)->toBe("ruby -v");
+    expect($command)->toBe('ruby -v');
 });
 
 it('can add multiple flags', function () {
-    $command = ShellBuilderClass::create("ls")
-                    ->withFlag("-l")
-                    ->withFlag("-a")
-                    ->getExecuteCommand();
+    $command = ShellBuilderClass::create('ls')
+        ->withFlag('-l')
+        ->withFlag('-a')
+        ->getExecuteCommand();
 
-    expect($command)->toBe("ls -l -a");
+    expect($command)->toBe('ls -l -a');
 });
 
 it('can add option', function () {
-    $command = ShellBuilderClass::create("ruby")
-                    ->withFlag("-v")
-                    ->withOption('-e', 'puts "Hello World"')
-                    ->getExecuteCommand();
+    $command = ShellBuilderClass::create('ruby')
+        ->withFlag('-v')
+        ->withOption('-e', 'puts "Hello World"')
+        ->getExecuteCommand();
 
     expect($command)->toBe('ruby -v -e puts "Hello World"');
 });
