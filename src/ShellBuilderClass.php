88368b0d80db7a52c8c3b6f27af813d9b41768ad
Fernando <fernando93d@gmail.com>
2023-06-22 21:11:28 -0600

    Add arguments, envs, and subcomands

--- src/ShellBuilderClass.php
+++ src/ShellBuilderClass.php
@@ -2,6 +2,8 @@
 
 namespace Fernandokbs\ShellBuilder;
 
+use Closure;
+
 class ShellBuilderClass
 {
     protected string $binary;
@@ -10,7 +12,11 @@ class ShellBuilderClass
 
     protected array $options = [];
 
-    public function __construct($binary = 'bash')
+    protected array $arguments = [];
+
+    protected array $environmentVariables = [];
+
+    public function __construct($binary = 'php')
     {
         $this->binary = $binary;
     }
@@ -27,6 +33,15 @@ class ShellBuilderClass
         return $this;
     }
 
+    public function withFlags(array $flags): self
+    {
+        foreach ($flags as $flag) {
+            $this->withFlag($flag);
+        }
+
+        return $this;
+    }
+
     public function withOption(string $option, string $value): self
     {
         $this->options[] = "{$option} {$value}";
@@ -34,17 +49,85 @@ class ShellBuilderClass
         return $this;
     }
 
-    public function getExecuteCommand(): string
+    public function withOptions(array $options): self
     {
-        $flags = implode(' ', $this->getFlags());
+        foreach ($options as $option => $value) {
+            $this->withOption($option, $value);
+        }
 
-        $options = implode(' ', $this->options);
-
-        return trim("{$this->binary} {$flags} {$options}");
+        return $this;
     }
 
-    protected function getFlags(): array
+    public function withArgument(string $argument): self
     {
-        return array_values($this->flags);
+        $this->arguments[] = $argument;
+
+        return $this;
+    }
+
+    public function withArguments(array $arguments): self
+    {
+        foreach ($arguments as $argument) {
+            $this->withArgument($argument);
+        }
+
+        return $this;
+    }
+
+    public function withEnvironmentVariable(string $variable, string $value): self
+    {
+        $this->environmentVariables[] = "{$variable}={$value}";
+
+        return $this;
+    }
+
+    public function withEnvironmentVariables(array $variables): self
+    {
+        foreach ($variables as $variable => $value) {
+            $this->withEnvironmentVariable($variable, $value);
+        }
+
+        return $this;
+    }
+
+    public function withSubCommand($command, Closure $callback = null): self
+    {
+        $builder = static::create($command);
+
+        if($callback) {
+            $callback($builder);
+        }
+
+        $this->options[] = $builder->getExecuteCommand();
+
+        return $this;
+    }
+
+    public function getExecuteCommand(): string
+    {
+        $command = "{$this->binary}";
+
+        $flags = implode(' ', $this->flags);
+        $options = implode(' ', $this->options);
+        $arguments = implode(' ', $this->arguments);
+        $environmentVariables = implode(' ', $this->environmentVariables);
+
+        if($environmentVariables) {
+            $command = "{$environmentVariables} {$command}";
+        }
+
+        if($flags) {
+            $command .= " {$flags}";
+        }
+
+        if($options) {
+            $command .= " {$options}";
+        }
+
+        if($arguments) {
+            $command .= " {$arguments}";
+        }
+
+        return $command;
     }
 }
--- tests/Pest.php
+++ tests/Pest.php
@@ -11,10 +11,9 @@ it('can add flag', function () {
 });
 
 it('can add multiple flags', function () {
-    $command = ShellBuilderClass::create('ls')
-        ->withFlag('-l')
-        ->withFlag('-a')
-        ->getExecuteCommand();
+    $command = ShellBuilderClass::create("ls")
+                    ->withFlags(['-l', '-a'])
+                    ->getExecuteCommand();
 
     expect($command)->toBe('ls -l -a');
 });
@@ -26,4 +25,70 @@ it('can add option', function () {
         ->getExecuteCommand();
 
     expect($command)->toBe('ruby -v -e puts "Hello World"');
+});
+
+it('can add multiple options', function () {
+    $command = ShellBuilderClass::create("gpg")
+                    ->withOptions([
+                        '--recipient' => 'fernando93d@gmail.com',
+                        '--sign' => './doc.txt'
+                    ])
+                    ->getExecuteCommand();
+
+    expect($command)->toBe('gpg --recipient fernando93d@gmail.com --sign ./doc.txt');
+});
+
+it('can add argument', function () {
+    $command = ShellBuilderClass::create("diff")
+                    ->withArgument('./file1.txt')
+                    ->getExecuteCommand();
+
+    expect($command)->toBe('diff ./file1.txt');
+});
+
+it('can add multiple arguments', function () {
+    $command = ShellBuilderClass::create("diff")
+                    ->withArguments(['./file1.txt', './file2.txt'])
+                    ->getExecuteCommand();
+
+    expect($command)->toBe('diff ./file1.txt ./file2.txt');
+});
+
+it('can add environment variable', function () {
+    $command = ShellBuilderClass::create("php")
+                    ->withEnvironmentVariable('APP_ENV', 'testing')
+                    ->getExecuteCommand();
+
+    expect($command)->toBe('APP_ENV=testing php');
+});
+
+it('can add multiple environment variables', function () {
+    $command = ShellBuilderClass::create("php")
+                    ->withEnvironmentVariables([
+                        'APP_ENV' => 'testing',
+                        'APP_DEBUG' => 'true'
+                    ])
+                    ->getExecuteCommand();
+
+    expect($command)->toBe('APP_ENV=testing APP_DEBUG=true php');
+});
+
+it('can add sub command', function () {
+    $command = ShellBuilderClass::create("git")
+                    ->withFlag('--no-pager')
+                    ->withSubCommand('log')
+                    ->getExecuteCommand();
+
+    expect($command)->toBe('git --no-pager log');
+});
+
+it('can add sub command with arguments', function () {
+    $command = ShellBuilderClass::create("git")
+                    ->withFlag('--no-pager')
+                    ->withSubCommand('log', function ($builder) {
+                        $builder->withFlag('--oneline');
+                    })
+                    ->getExecuteCommand();
+
+    expect($command)->toBe('git --no-pager log --oneline');
 });
